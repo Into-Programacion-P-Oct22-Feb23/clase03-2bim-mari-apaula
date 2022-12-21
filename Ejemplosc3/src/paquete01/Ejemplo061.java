0ed2d97352df03e5f93d457c02bf71ac580450e8
Maria Paula Montaño <mpmontano1@utpl.edu.ec>
2022-12-21 15:25:36 -0500

    moistrar la suma de los arreglos pares

---
+++ Ejemplosc3/src/paquete01/Ejemplo061.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete01;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo061 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Creación de arreglo bidimensionales
+        
+        int[][] arreglo1 = {{10, 41, 40}, {1, 2, 3}};
+        /*
+        10      41      40
+        1       2       3
+        */
+        int suma = 0;
+        int valor;
+        String cadena = "";
+        for (int fila = 0; fila < arreglo1.length; fila++) {
+            for (int col = 0; col < arreglo1[fila].length; col++) {
+                valor = arreglo1[fila][col];
+                if (fila==col){
+                    suma = suma + valor;
+                    cadena = String.format("%s%d\n", cadena, 
+                            arreglo1[fila][col]);
+                }
+                
+            }
+        }
+        System.out.printf("la suma de:\n%sSuma de valores del arreglo: %d\n",
+                            cadena,suma);
+    }
+    
+}
